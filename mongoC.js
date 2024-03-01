575d2c6607d085a092d101fbbd894594ab74efb4
Arun Kushwaha <er.arunkushwaha@gmail.com>
2024-03-01 23:07:56 +0545

    deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,14 @@
 import { MongoClient } from "mongodb";
 
-const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const password = encodeURIComponent(process.env.MONGO_PWD.trim());
+// clustore url
+const connectionString = `mongodb+srv://testMern:${password}@testmern.kwumbm8.mongodb.net/?retryWrites=true&w=majority&appName=testMern`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
